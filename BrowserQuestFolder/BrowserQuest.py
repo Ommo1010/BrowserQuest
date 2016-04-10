842011196c7659a2e550f9b5a658797011ce85d0
Ommo1010 <oguzhanyilma@gmail.com>
2016-04-10 13:29:33 -0400

    Absolute Garbage

--- BrowserQuestFolder/BrowserQuest.py
+++ BrowserQuestFolder/BrowserQuest.py
@@ -9,22 +9,31 @@ screen = display.set_mode((900,450),flags,32)
 #------------------------------------
 init()                                  
 mixer.music.load("Sound\Music.mp3")      
-mixer.music.play(-1)#plays the song indefinitely
+#mixer.music.play(-1)#plays the song indefinitely
 
 font.init()
 #-----------------------------------
+sandtile=transform.smoothscale(image.load("Map\Sand.png"),(30,30)).convert()
 greentile=transform.smoothscale(image.load("Map\grass.png"),(30,30)).convert()
 #------------------------------------
 spawnmap=[[0 for x in range(30)] for x in range(15)]
+ospawnmap=spawnmap
+sandmap=[[0 for x in range(30)] for x in range(15)]
+osandmap=sandmap
 #spawnmap[5][6]=1
 #------------------------------------
 running=True
 spawn=True
-curpos=0,0
+curpos=300,300
 move=False
 movecounter=0
 enter=True
 breath=3
+test=False
+first=True
+erase=True
+sand=False
+entitycounter=10
 #------------------------------------
 def pathfinder(new,old,mapp):
     if new==old:
@@ -73,17 +82,66 @@ while running:
     if enter==True:
         spawnmap=entity(spawnmap,5,"bandit")
         enter=False
-    if spawn==True:
-        for l in range(0,30):
-            for w in range(0,15):
+    if curpos[0]//30==29:
+        sandmap=osandmap
+        curpos=300,300
+        enter=True
+        spawn=False
+        sand=True
+    elif curpos[0]//30==0:
+        spawnmap=ospawnmap
+        curpos=300,300
+        enter=True
+        spawn=True
+        sand=False
+    #---------------------------------
+    """
+    if curpos[0]//30<29 and curpos[1]//30<14 and spawnmap[curpos[1]//30+1][curpos[0]//30]==2:
+        test=True
+    entitycounter+=1
+    if test==True and entitycounter%10==0:
+        entitycounter=0
+        if first==True:
+            entitypos=pathfinder(curpos,(curpos[0]+1,curpos[1]),spawnmap)
+            first=False
+        else:
+            entitypos=pathfinder(curpos,entitypos,spawnmap)
+        if erase==True:
+            erase=False
+            oldpos=curpos[1]//30+1,curpos[0]//30
+        elif erase==False:
+            erase=True
+        spawnmap[oldpos[0]][oldpos[1]]=0
+        spawnmap[entitypos[1]//30][entitypos[0]//30]=2
+    #--------------------------------
+    if curpos[0]//30<29 and curpos[1]//30<14 and spawnmap[curpos[1]//30-1][curpos[0]//30]==2:
+        pass
+    if curpos[0]//30<29 and curpos[1]//30<14 and spawnmap[curpos[1]//30][curpos[0]//30+1]==2:
+        pass
+    if curpos[0]//30<29 and curpos[1]//30<14 and spawnmap[curpos[1]//30][curpos[0]//30-1]==2:
+        pass
+    #-------------------------------
+    """
+    for l in range(0,30):
+        for w in range(0,15):
+            if sand==True:
+                if spawnmap[w][l]==0:
+                    screen.blit(sandtile,(l*30,w*30))
+                if spawnmap[w][l]==2:
+                    draw.rect(screen,(255,0,0),(l*30,w*30,30,30),0)
+            if spawn==True:
                 if spawnmap[w][l]==0:
                     screen.blit(greentile,(l*30,w*30))
                 if spawnmap[w][l]==2:
+                    """
+                    #################
                     if breath==3:
                         breath=0
                     elif breath==0:
                         breath=3
-                    draw.rect(screen,(255,0,0),(l*30,w*30+breath,30,30),0)
+                    ################
+                    """
+                    draw.rect(screen,(255,0,0),(l*30,w*30,30,30),0)
 
     mappos=mx//30*30,my//30*30
     if mb[0]==1 and spawnmap[mappos[1]//30][mappos[0]//30]==0:
@@ -94,7 +152,7 @@ while running:
         draw.rect(screen,(0,255,0),(mappos[0],mappos[1],30,30),1)
     draw.rect(screen,(0,0,255),(curpos[0],curpos[1],30,30),0)
     movecounter+=1
-    if move==True and movecounter%5==0:
+    if move==True and movecounter%7==0:
         movecounter=0
         if curpos==mappos:
             move=False
