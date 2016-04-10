2f2b3984255fc30151ec03f25c5b214c1325a5a2
Ommo1010 <oguzhanyilma@gmail.com>
2016-04-10 11:51:49 -0400

    Entity Start

--- BrowserQuestFolder/BrowserQuest.py
+++ BrowserQuestFolder/BrowserQuest.py
@@ -1,42 +1,65 @@
-#30x14(plus 1 for utilities)
 #------------------------------------
 from pygame import *
 from math import *
 from random import *
+from pprint import *
 #------------------------------------
 flags=DOUBLEBUF|HWSURFACE#|FULLSCREEN
 screen = display.set_mode((900,450),flags,32)
 #------------------------------------
 init()                                  
 mixer.music.load("Sound\Music.mp3")      
-#mixer.music.play(-1)#plays the song indefinitely
+mixer.music.play(-1)#plays the song indefinitely
 
 font.init()
-#------------------------------------
+#-----------------------------------
 greentile=transform.smoothscale(image.load("Map\grass.png"),(30,30)).convert()
 #------------------------------------
 spawnmap=[[0 for x in range(30)] for x in range(15)]
-spawnmap[5][6]=1
+#spawnmap[5][6]=1
 #------------------------------------
 running=True
 spawn=True
 curpos=0,0
 move=False
 movecounter=0
+enter=True
+breath=3
 #------------------------------------
-def pathfinder(new,old):
+def pathfinder(new,old,mapp):
     if new==old:
-        return curpos
+        return new
+    #-----------------------------
+    """
+    curx,cury=old[1]//30,old[0]//30
+    if 0>curx or curx>29 or cury<0 or cury>14:
+        subtract=0
+    else:
+        subtract=1
+    """
+    #-----------------------------
     if old[0]!=new[0]:
-        if new[0]>old[0]:
+        if new[0]>old[0]:# and mapp[curx-subtract][cury]==0:
             return(new[0]-30,new[1])
-        if new[0]<old[0]:
+        if new[0]<old[0]:# and mapp[curx+subtract][cury]==0:
             return(new[0]+30,new[1])
     if old[1]!=new[1]:
-        if new[1]>old[1]:
+        if new[1]>old[1]:# and mapp[curx][cury-subtract]==0:
             return(new[0],new[1]-30)
-        if new[1]<old[1]:
+        if new[1]<old[1]:# and mapp[curx][cury+subtract]==0:
             return(new[0],new[1]+30)
+    return new
+#------------------------------------
+def entity(mapp,number,clas):
+    for i in range(number):
+        place=randint(0,451)
+        column=place//15-1
+        row=place%15-1
+        if clas=="bandit":
+            mapp[row][column]=2
+        elif clas=="goblin":
+            mapp[row][column]=3
+    return mapp
 #------------------------------------
 while running:
     for e in  event.get():
@@ -47,11 +70,21 @@ while running:
                 running = False
     mb = mouse.get_pressed()
     mx,my = mouse.get_pos()
+    if enter==True:
+        spawnmap=entity(spawnmap,5,"bandit")
+        enter=False
     if spawn==True:
         for l in range(0,30):
             for w in range(0,15):
                 if spawnmap[w][l]==0:
                     screen.blit(greentile,(l*30,w*30))
+                if spawnmap[w][l]==2:
+                    if breath==3:
+                        breath=0
+                    elif breath==0:
+                        breath=3
+                    draw.rect(screen,(255,0,0),(l*30,w*30+breath,30,30),0)
+
     mappos=mx//30*30,my//30*30
     if mb[0]==1 and spawnmap[mappos[1]//30][mappos[0]//30]==0:
         draw.rect(screen,(255,0,0),(mappos[0],mappos[1],30,30),1)
@@ -65,6 +98,7 @@ while running:
         movecounter=0
         if curpos==mappos:
             move=False
-        curpos=pathfinder(curpos,omappos)
+        curpos=pathfinder(curpos,omappos,spawnmap)
+    
     display.flip()
 quit()
